f511169fcf7167162b6039da73497599a2de0afd
BakaUsopp <ygajera939@gmail.com>
2023-09-17 17:10:20 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.example.entities.Employee;
+import org.example.persistence.CustomPersistenceUnitInfo;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String puName = "pu-name";
+        Map<String,String> props = new HashMap<>();
+        props.put("hibernate.show_sql", "true");
+        props.put("hibernate.hbm2ddl.auto", "create"); // create ,update, none
+
+        EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(puName), props);
+
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            var e1 = new Employee();
+//            e1.setId(1);
+            e1.setName("Usopp");
+            e1.setAddress("Ohio");
+
+            em.persist(e1);
+
+
+            //find vs getReference -> getReference win
+
+//            var e1 = em.find(Employee.class,1);
+//            var e1 = em.getReference(Employee.class,1);
+//            e1.setName("Luffy");
+//            System.out.println("before, "+e1);
+//
+//            em.refresh(e1); //undo last value/reference
+//            System.out.println("after, " +e1);
+
+
+//            em.persist();   -> Adding an entity in the context
+//            em.find()       -> Finds by PK. Get from DB and add it to the context if it doesn't already exist
+//            em.remove();    -> Marking entity for removal
+//            em.merge();     -> Merges an entity from outside the context to the context.
+//            em.refresh();   -> Mirror the context from the database
+//            em.detach();    -> Taking the entity out of the context
+//            em.getReference()
+
+            em.getTransaction().commit(); // end of transaction
+        } finally {
+            em.close();
+        }
+    }
+
+}
